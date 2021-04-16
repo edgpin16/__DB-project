948670647a9cd566160c8dd305cdf48d19a7b4d3
Edgardo <edgardo.pinto16@gmail.com>
2021-04-16 17:24:46 -0400

    A#adiendo la conexion a la base de datos

---
+++ src/database.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql');
+const {promisify} = require('util');
+const {database} = require('./keys');
+
+const pool = mysql.createPool(database);
+
+pool.getConnection((error, connection) => {
+    if(error)
+        if(error.code === 'PROTOCOL_CONNECTION_LOST')
+            console.error("Conexión con la bases de datos cerrada");
+        else if(error.code === 'ER_CON_COUNT_ERROR')
+            console.error("La base de datos tiene muchas conexiones");
+        else if(error.code === 'ECONNREFUSED')
+            console.error("Conexión con la bases de datos rechazada");
+    else if(connection){
+        connection.release();
+        console.log("Base de datos conectada");
+    }
+    return;
+});
+
+module.exports = pool;
