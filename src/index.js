d3136216aa11f5566cc6b33640d26a53aecc20b2
Edgardo <edgardo.pinto16@gmail.com>
2021-04-16 14:14:32 -0400

    Add lib/handlebars.js file

--- src/index.js
+++ src/index.js
@@ -16,19 +16,29 @@ app.engine('.hbs', expressHBS({
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
+    helpers: require('./lib/handlebars'),
 }));
+app.set('view engine', '.hbs');
 
 //middlewares
 
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended : false}));
+app.use(express.json());
 
 //global variables
+
+app.use((req, res, next) => {
+    next();
+});
 
 //routes
 
 app.use(require('./routes'));
 
 //public 
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 //starting the server
 
