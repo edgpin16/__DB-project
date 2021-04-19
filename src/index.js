4915db74646c571781bed5c07e3289acd83f9da9
Edgardo <edgardo.pinto16@gmail.com>
2021-04-19 16:43:14 -0400

    Add solicitud de pedido a la bd

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     app.locals.success = req.flash('success');
+    app.locals.error = req.flash('error');
     next();
 });
 
--- src/routes/solicitudPedido.js
+++ src/routes/solicitudPedido.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const date = new Date();
 
 const pool = require('../database');
 
@@ -11,45 +12,72 @@ router.get('/agregar', async (req, res) => {
     try{
         const laboratorios = await pool.query('SELECT * FROM laboratorio');
         const farmacias = await pool.query('SELECT * FROM farmacia');
-        const analistas = await pool.query('SELECT * FROM (empleado RIGHT JOIN analista ON empleado.CI = analista.CI_Empleado)');
-        res.render('solicitud_pedido/agregar', {laboratorios, farmacias, analistas});
+        //const analistas = await pool.query('SELECT * FROM (empleado RIGHT JOIN analista ON empleado.CI = analista.CI_Empleado)');
+        res.render('solicitud_pedido/agregar', {laboratorios, farmacias});
     }
     catch(error){
         console.log("Ocurrio un error :(", error);
     }
 });
 
-router.post('/agregar', async (req, res) => {
-    try {
-        const {ID_Farmacia, ID_Laboratorio, CI_Analista} = req.body;
-        const [{ID_Farmacia: busquedaFarmacia, nombre:analista}] = await pool.query(`SELECT ID_Farmacia, nombre FROM empleado WHERE empleado.CI = ${CI_Analista}`);
-        
-        if (parseInt(ID_Farmacia, 10) === busquedaFarmacia){
-            const newSolicitudPedido = {
-                ID_Farmacia,
-                ID_Laboratorio,
-                analista,
-            }
-            await pool.query('INSERT INTO farmacia_laboratorio_solicitud_pedido set ?', [newSolicitudPedido]);
-            res.send('Recibido');
-        }
-        else{
-            req.flash('success', 'Analista no pertenece a la farmacia.');
-            res.redirect('/solicitud_pedido/agregar')
-        }
+router.get('/agregarAnalista', async (req, res) => {
+    try{
+        const {ID_Farmacia, ID_Laboratorio} = req.query;
+        const farmacia = await pool.query(`SELECT * FROM farmacia WHERE ID = ${ID_Farmacia}`);
+        const laboratorio = await pool.query(`SELECT * FROM laboratorio WHERE ID = ${ID_Laboratorio}`);
+        const analistasFarmacia = await pool.query(`SELECT * FROM (empleado RIGHT JOIN analista ON empleado.CI = analista.CI_Empleado) WHERE empleado.ID_Farmacia = ${ID_Farmacia}`);
+        res.render('solicitud_pedido/agregarAnalista', {laboratorio, farmacia, analistasFarmacia});
     }
-    catch (error) {
-        console.error("Ha ocurrido un error :(", error)
+    catch(error){
+        console.log("Ocurrio un error :(", error);
     }
 });
 
-router.get('/agregarAnalista', async (req, res) => {
+router.post('/agregarAnalista', async (req, res) => {
     try{
-        res.render('solicitud_pedido/agregarAnalista');
+        const {ID_Farmacia, ID_Laboratorio, analista, tipo_pago} = req.body;
+        const newSolicitud = {
+            ID_Farmacia,
+            ID_Laboratorio,
+            analista,
+        }
+        await pool.query('INSERT INTO farmacia_laboratorio_solicitud_pedido set ?', [newSolicitud]);
+
+        const [{ID}] = await pool.query(`SELECT ID from farmacia_laboratorio_solicitud_pedido WHERE analista = \'${analista}\' AND ID_Laboratorio = ${ID_Laboratorio} AND ID_Farmacia = ${ID_Farmacia}`);
+        const newPedido = {
+            ID_Solicitud_pedido : ID,
+            forma_pago : tipo_pago
+        }
+        await pool.query('INSERT INTO pedido set ? ', [newPedido]);
+        res.send('agregado');
     }
     catch(error){
-        console.error("Ha ocurrido un error :(", error);
+        console.log("Ocurrio un error :(", error);
     }
 });
+
+// router.post('/agregar', async (req, res) => {
+//     try {
+//         const {ID_Farmacia, ID_Laboratorio, CI_Analista} = req.body;
+//         const [{ID_Farmacia: busquedaFarmacia, nombre:analista}] = await pool.query(`SELECT ID_Farmacia, nombre FROM empleado WHERE empleado.CI = ${CI_Analista}`);
+        
+//         if (parseInt(ID_Farmacia, 10) === busquedaFarmacia){
+//             const newSolicitudPedido = {
+//                 ID_Farmacia,
+//                 ID_Laboratorio,
+//                 analista,
+//             }
+//             await pool.query('INSERT INTO farmacia_laboratorio_solicitud_pedido set ?', [newSolicitudPedido]);
+//             res.send('Recibido');
+//         }
+//         else{
+//             req.flash('error', 'Analista no pertenece a la farmacia.');
+//             res.redirect('/solicitud_pedido/agregar')
+//         }
+//     }
+//     catch (error) {
+//         console.error("Ha ocurrido un error :(", error)
+//     }
+// });
 
 module.exports = router;
